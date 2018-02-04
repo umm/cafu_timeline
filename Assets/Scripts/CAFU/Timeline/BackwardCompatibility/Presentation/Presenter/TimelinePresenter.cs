52917d7c8a24e97bb93d88fca67452116176ef1f
Tetsuya Mori <monry84@gmail.com>
2018-02-04 20:20:27 +0900

    :sparkles: Add backward compatibilities
    * 後方互換用に Obsolete 属性を付けたクラスとして既存クラスを退避

---
+++ Assets/Scripts/CAFU/Timeline/BackwardCompatibility/Presentation/Presenter/TimelinePresenter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using CAFU.Timeline.Domain.Model;
+using CAFU.Timeline.Domain.UseCase;
+using UnityEngine.Playables;
+using UnityModule.Playables;
+using Object = UnityEngine.Object;
+#pragma warning disable 618
+
+namespace CAFU.Timeline.Presentation.Presenter {
+
+    [Obsolete("Please use ITimelinePresenter<TTimelineModel> instead of this interface.")]
+    public interface ITimelinePresenter<TEnum, TTimelineInformation>
+        where TEnum : struct
+        where TTimelineInformation : TimelineInformation<TEnum>, new() {
+
+        TimelineUseCase<TEnum, TTimelineInformation> TimelineUseCase { get; }
+
+    }
+
+    public static class TimelinePresenterExtensionBackwardCompatibility {
+
+        [Obsolete("Please use RegisterPlayableDirectorResolver<TTimelineModel>(this ITimelinePresenter<TTimelineModel> presenter) instead of this extension method.")]
+        public static void RegisterPlayableDirectorResolver<TEnum, TTimelineInformation>(this ITimelinePresenter<TEnum, TTimelineInformation> self, IPlayableDirectorResolver<TEnum> playableDirectorResolver) where TEnum : struct where TTimelineInformation : TimelineInformation<TEnum>, new() {
+            self.TimelineUseCase.RegisterPlayableDirectorResolver(playableDirectorResolver);
+        }
+
+        [Obsolete("Please use GetPlayableDirector<TEnum, TTimelineModel>(this ITimelinePresenter<TTimelineModel> presenter, TEnum name) instead of this extension method.")]
+        public static PlayableDirector GetPlayableDirector<TEnum, TTimelineInformation>(this ITimelinePresenter<TEnum, TTimelineInformation> self, TEnum name) where TEnum : struct where TTimelineInformation : TimelineInformation<TEnum>, new() {
+            return self.TimelineUseCase.GetPlayableDirector(name);
+        }
+
+        [Obsolete("Please use SetGenericBindingByTrackName<TEnum, TTimelineModel, TValue>(this ITimelinePresenter<TTimelineModel> presenter, TEnum name, string trackName, TValue value) instead of this extension method.")]
+        public static void SetGenericBindingByTrackName<TEnum, TTimelineInformation, TValue>(this ITimelinePresenter<TEnum, TTimelineInformation> self, TEnum name, string trackName, TValue value) where TEnum : struct where TTimelineInformation : TimelineInformation<TEnum>, new() where TValue: Object {
+            self.GetPlayableDirector(name).SetGenericBindingByTrackName(trackName, value);
+        }
+
+        [Obsolete("Please use SetGenericBindingByPlayableAssetName<TEnum, TTimelineModel, TValue>(this ITimelinePresenter<TTimelineModel> presenter, TEnum name, string playableAssetName, TValue value) instead of this extension method.")]
+        public static void SetGenericBindingByPlayableAssetName<TEnum, TTimelineInformation, TValue>(this ITimelinePresenter<TEnum, TTimelineInformation> self, TEnum name, string playableAssetName, TValue value) where TEnum : struct where TTimelineInformation : TimelineInformation<TEnum>, new() where TValue: Object {
+            self.GetPlayableDirector(name).SetGenericBindingByPlayableAssetName(playableAssetName, value);
+        }
+
+        [Obsolete("Please use SetGenericBindingByPlayableAssetName<TEnum, TTimelineModel, TValue>(this ITimelinePresenter<TTimelineModel> presenter, TEnum name, string trackName, string playableAssetName, TValue value) instead of this extension method.")]
+        public static void SetGenericBindingByTrackNameAndPlayableAssetName<TEnum, TTimelineInformation, TValue>(this ITimelinePresenter<TEnum, TTimelineInformation> self, TEnum name, string trackName, string playableAssetName, TValue value) where TEnum : struct where TTimelineInformation : TimelineInformation<TEnum>, new() where TValue: Object {
+            self.GetPlayableDirector(name).SetGenericBindingByTrackNameAndPlayableAssetName(trackName, playableAssetName, value);
+        }
+
+        [Obsolete("Please use SetReferenceValueByTrackName<TEnum, TTimelineModel, TValue>(this ITimelinePresenter<TTimelineModel> presenter, TEnum name, string trackName, TValue value) instead of this extension method.")]
+        public static void SetReferenceValueByTrackName<TEnum, TTimelineInformation, TValue>(this ITimelinePresenter<TEnum, TTimelineInformation> self, TEnum name, string trackName, TValue value) where TEnum : struct where TTimelineInformation : TimelineInformation<TEnum>, new() where TValue: Object {
+            self.GetPlayableDirector(name).SetReferenceValueByTrackName(trackName, value);
+        }
+
+        [Obsolete("Please use SetReferenceValueByPlayableAssetName<TEnum, TTimelineModel, TValue>(this ITimelinePresenter<TTimelineModel> presenter, TEnum name, string playableAssetName, TValue value) instead of this extension method.")]
+        public static void SetReferenceValueByPlayableAssetName<TEnum, TTimelineInformation, TValue>(this ITimelinePresenter<TEnum, TTimelineInformation> self, TEnum name, string playableAssetName, TValue value) where TEnum : struct where TTimelineInformation : TimelineInformation<TEnum>, new() where TValue: Object {
+            self.GetPlayableDirector(name).SetReferenceValueByPlayableAssetName(playableAssetName, value);
+        }
+
+        [Obsolete("Please use SetReferenceValueByTrackNameAndPlayableAssetName<TEnum, TTimelineModel, TValue>(this ITimelinePresenter<TTimelineModel> presenter, TEnum name, string trackName, string playableAssetName, TValue value) instead of this extension method.")]
+        public static void SetReferenceValueByTrackNameAndPlayableAssetName<TEnum, TTimelineInformation, TValue>(this ITimelinePresenter<TEnum, TTimelineInformation> self, TEnum name, string trackName, string playableAssetName, TValue value) where TEnum : struct where TTimelineInformation : TimelineInformation<TEnum>, new() where TValue: Object {
+            self.GetPlayableDirector(name).SetReferenceValueByTrackNameAndPlayableAssetName(trackName, playableAssetName, value);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/CAFU/Timeline/BackwardCompatibility/Presentation/View/TimelineView.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CAFU.Timeline.Domain.Model;
+using CAFU.Timeline.Domain.UseCase;
+using CAFU.Timeline.Presentation.Presenter;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace CAFU.Timeline.Presentation.View {
+
+    [Obsolete("Please use TimelineView<TTimelineModel> instead of this class.")]
+    public abstract class TimelineView<TEnum, TTimelineInformation> : MonoBehaviour, ITimelineView, IPlayableDirectorResolver<TEnum>
+        where TEnum : struct
+        where TTimelineInformation : TimelineInformation<TEnum>, new() {
+
+        [SerializeField]
+        private List<TTimelineInformation> timelineInformationList;
+
+        private List<TTimelineInformation> TimelineInformationList => this.timelineInformationList;
+
+        protected abstract ITimelinePresenter<TEnum, TTimelineInformation> GetTimelinePresenter();
+
+        private void Awake() {
+            this.GetTimelinePresenter().RegisterPlayableDirectorResolver(this);
+        }
+
+        public PlayableDirector GetPlayableDirector(TEnum timelineName) {
+            if (!this.TimelineInformationList.Any(x => x.Name.Equals(timelineName))) {
+                this.AddTimelineInformationFromTransform(timelineName);
+            }
+            return this.TimelineInformationList.Find(x => x.Name.Equals(timelineName)).PlayableDirector;
+        }
+
+        private void AddTimelineInformationFromTransform(TEnum timelineName) {
+            // enum のアンダースコアをスラッシュに置換して、Hierarchy を探す
+            Transform playableDirectorTransform = this.transform.Find(timelineName.ToString().Replace("_", "/"));
+            // 見付からなかった場合に、配下の全 Transform の名前を enum の完全一致で探す
+            if (playableDirectorTransform == default(Transform)) {
+                playableDirectorTransform = this.transform.GetComponentsInChildren<Transform>(true).ToList().Find(x => x.name == timelineName.ToString());
+            }
+            if (playableDirectorTransform != default(Transform)) {
+                this.TimelineInformationList.Add(
+                    new TTimelineInformation() {
+                        Name = timelineName,
+                        PlayableDirector = playableDirectorTransform.GetComponent<PlayableDirector>(),
+                    }
+                );
+            }
+        }
+
+        public UniRx.IObservable<Unit> OnAwakeAsObservable() {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
